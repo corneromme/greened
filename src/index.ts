ea9c782fc5894ff13875d0a32a0d3d7a6d5bb4dc
corneromme <info@corneromme.com>
2024-07-22 12:04:03 +0200

    chore: Add Lenis smooth scrolling and update dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,39 @@
-import { greetUser } from '$utils/greet';
+// Stylesheets import
+import './buttons.css';
+import './lenis.css';
 
-window.Webflow ||= [];
-window.Webflow.push(() => {
-  const name = 'John Doe';
-  greetUser(name);
-});
+// Import dependencies
+import gsap from 'gsap';
+import ScrollTrigger from 'gsap/ScrollTrigger';
+import SplitText from 'gsap/SplitText';
+import Lenis from 'lenis';
+
+gsap.registerPlugin(ScrollTrigger, SplitText);
+
+// Init scripts
+function initScripts() {
+  initLenis();
+}
+
+initScripts();
+
+// Lenis Smooth Scroll
+function initLenis() {
+  const lenis = new Lenis();
+
+  lenis.on('scroll', ScrollTrigger.update);
+
+  gsap.ticker.add((time) => {
+    lenis.raf(time * 1000);
+  });
+
+  gsap.ticker.lagSmoothing(0);
+}
+
+// Hero Appear Animation
+gsap.set('.hero_left_wrap .heading-h1', { opacity: 0 });
+gsap.fromTo(
+  '.hero_left_wrap .heading-h1',
+  { y: 100 },
+  { y: 0, opacity: 1, duration: 1, ease: 'power4.out', delay: 0.5 }
+);
